7a3d66ad9f49050efb41146768f9af387ad014a7
Aammad <aammad@codilityhub.com>
2026-02-27 11:09:32 +0500

    fixed issue of redirecting to /resources without origin

--- resources/js/mixin/global.js
+++ resources/js/mixin/global.js
@@ -1,7 +1,7 @@
 export const global = {
     methods: {
         redirect(locale) {
-            window.location = this.replaceUrlParam(this.field.url + '/resources/' + this.resourceName + "/" + this.field.value.id + "/edit", 'lang', locale);
+            window.location = this.replaceUrlParam(window.location.origin + '/resources/' + this.resourceName + "/" + this.field.value.id + "/edit", 'lang', locale);
         },
 
         replaceUrlParam(url, paramName, paramValue) {
